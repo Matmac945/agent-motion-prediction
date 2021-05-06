17be733a2a9d5a32e5c423900b2e0008f8faea37
Matmac945 <macias945@gmail.com>
2021-05-05 23:37:12 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='kaggle challenge Agent Motion Prediction',
+    author='Matias Macias Gomez',
+    license='MIT',
+)
